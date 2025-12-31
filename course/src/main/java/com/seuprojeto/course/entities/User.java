0b74d0da07aafce00f2b2ce901f4d4b24273276f
GUGA-CRYPTO <gu.santos251@gmail.com>
2025-12-31 11:33:55 -0300

    H2 Database, test profile, jpa

--- course/src/main/java/com/seuprojeto/course/entities/User.java
+++ course/src/main/java/com/seuprojeto/course/entities/User.java
@@ -1,9 +1,18 @@
 package com.seuprojeto.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
+import jakarta.persistence.Entity;
 
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -20,6 +29,8 @@ public class User implements Serializable {
         this.phone = phone;
         this.password = password;
     }
+
+
 
 
     public Long getId() {
